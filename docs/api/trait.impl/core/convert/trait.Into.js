ffa11ab29d01d87ac78d7ae162b96e1063c39650
Nigel Eke <nigeleke@users.noreply.github.com>
2024-06-10 11:27:24 +0000

    Release cribbage@0.2.16

---
+++ docs/api/trait.impl/core/convert/trait.Into.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"cribbage":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.usize.html\">usize</a>&gt; for <a class=\"struct\" href=\"cribbage/domain/prelude/struct.Rank.html\" title=\"struct cribbage::domain::prelude::Rank\">Rank</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()

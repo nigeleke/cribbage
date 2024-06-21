63b557ff0e5c0b655036c62b01a7b7a1b82e5ca7
Nigel Eke <nigeleke@users.noreply.github.com>
2024-06-21 03:07:22 +0000

    Release cribbage@0.2.24

--- docs/api/search-index.js
+++ docs/api/search-index.js
@@ -1,5 +1,5 @@
 var searchIndex = new Map(JSON.parse('[\
-["cribbage",{"t":"CH","n":["prelude","App"],"q":[[0,"cribbage"],[1,"cribbage::prelude"],[2,"leptos_dom"],[3,"cribbage::app"]],"i":[0,0],"f":"`{{}{{`{b}}}}","D":"b","p":[[10,"IntoView",2]],"r":[[1,3]],"b":[],"c":"OjAAAAAAAAA=","e":"OjAAAAEAAAAAAAEAEAAAAAAAAQA="}]\
+["cribbage",{"t":"H","n":["App"],"q":[[0,"cribbage"],[1,"leptos_dom"],[2,"cribbage::app"]],"i":[0],"f":"{{}{{`{b}}}}","D":"b","p":[[10,"IntoView",1]],"r":[[0,2]],"b":[],"c":"OjAAAAAAAAA=","e":"OjAAAAEAAAAAAAAAEAAAAAAA"}]\
 ]'));
 if (typeof exports !== 'undefined') exports.searchIndex = searchIndex;
 else if (window.initSearch) window.initSearch(searchIndex);

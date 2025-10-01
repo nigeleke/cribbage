e6b18c1a1ec92199e267466bc952328c82eac926
Nigel Eke <nigeleke@gmail.com>
2025-10-02 07:31:30 +1000

    Add back goofy-animals

--- packages/api/src/lib.rs
+++ packages/api/src/lib.rs
@@ -9,7 +9,6 @@ mod services;
 #[cfg(test)]
 mod test;
 
-pub(crate) use display::*;
 pub(crate) use domain::*;
 pub use server::*;
 pub use services::*;
--- packages/api/src/name_builder.rs
+++ packages/api/src/name_builder.rs
@@ -1,13 +1,12 @@
 use std::sync::LazyLock;
 
-// use goofy_animals::GoofyAnimals;
-// use rand::{Rng, SeedableRng};
-// use rand_chacha::ChaCha20Rng;
+use goofy_animals::GoofyAnimals;
+use rand::SeedableRng;
+use rand_chacha::ChaCha20Rng;
 
 pub fn generate_game_name() -> String {
-    "x".into()
-    // let mut rng = ChaCha20Rng::from_os_rng();
-    // GOOFY_ANIMALS.generate_name(&mut rng)
+    let mut rng = ChaCha20Rng::from_os_rng();
+    GOOFY_ANIMALS.generate_name(&mut rng)
 }
 
 static ANIMALS_TXT: &str = include_str!("../data/animals.txt");
@@ -17,5 +16,5 @@ static ANIMALS: LazyLock<Vec<&'static str>> = LazyLock::new(|| ANIMALS_TXT.lines
 
 static ADJECTIVES: LazyLock<Vec<&'static str>> = LazyLock::new(|| ADJECTIVES_TXT.lines().collect());
 
-// static GOOFY_ANIMALS: LazyLock<GoofyAnimals<'static>> =
-//     LazyLock::new(|| GoofyAnimals::new(ANIMALS.as_slice(), ADJECTIVES.as_slice()));
+static GOOFY_ANIMALS: LazyLock<GoofyAnimals<'static>> =
+    LazyLock::new(|| GoofyAnimals::new(ANIMALS.as_slice(), ADJECTIVES.as_slice()));
